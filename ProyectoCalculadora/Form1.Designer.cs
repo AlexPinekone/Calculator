bd0ce56c37c4f83746feaa37075dfcabfb314c62
AlexPinekone <127763929+AlexPinekone@users.noreply.github.com>
2024-09-11 19:49:18 -0600

    Add files via upload

---
+++ ProyectoCalculadora/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace ProyectoCalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.Button();
+            this.opMemoria = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.opDivi = new System.Windows.Forms.Button();
+            this.opMulti = new System.Windows.Forms.Button();
+            this.opResta = new System.Windows.Forms.Button();
+            this.opSuma = new System.Windows.Forms.Button();
+            this.opIgual = new System.Windows.Forms.Button();
+            this.cajaResultado = new System.Windows.Forms.TextBox();
+            this.listHistorial = new System.Windows.Forms.ListBox();
+            this.listMemoria = new System.Windows.Forms.ListBox();
+            this.opDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(32, 184);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(114, 58);
+            this.num7.TabIndex = 0;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(152, 184);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(114, 58);
+            this.num8.TabIndex = 1;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(272, 184);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(114, 58);
+            this.num9.TabIndex = 2;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(272, 248);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(114, 58);
+            this.num6.TabIndex = 5;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(152, 248);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(114, 58);
+            this.num5.TabIndex = 4;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(32, 248);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(114, 58);
+            this.num4.TabIndex = 3;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(272, 312);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(114, 58);
+            this.num3.TabIndex = 8;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(152, 312);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(114, 58);
+            this.num2.TabIndex = 7;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(32, 312);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(114, 58);
+            this.num1.TabIndex = 6;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // opMemoria
+            // 
+            this.opMemoria.Location = new System.Drawing.Point(152, 376);
+            this.opMemoria.Name = "opMemoria";
+            this.opMemoria.Size = new System.Drawing.Size(114, 58);
+            this.opMemoria.TabIndex = 10;
+            this.opMemoria.Text = "M+";
+            this.opMemoria.UseVisualStyleBackColor = true;
+            this.opMemoria.Click += new System.EventHandler(this.opMemoria_Click);
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(32, 376);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(114, 58);
+            this.num0.TabIndex = 9;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // opDivi
+            // 
+            this.opDivi.Location = new System.Drawing.Point(392, 376);
+            this.opDivi.Name = "opDivi";
+            this.opDivi.Size = new System.Drawing.Size(114, 58);
+            this.opDivi.TabIndex = 15;
+            this.opDivi.Text = "/";
+            this.opDivi.UseVisualStyleBackColor = true;
+            this.opDivi.Click += new System.EventHandler(this.opDivi_Click);
+            // 
+            // opMulti
+            // 
+            this.opMulti.Location = new System.Drawing.Point(392, 312);
+            this.opMulti.Name = "opMulti";
+            this.opMulti.Size = new System.Drawing.Size(114, 58);
+            this.opMulti.TabIndex = 14;
+            this.opMulti.Text = "*";
+            this.opMulti.UseVisualStyleBackColor = true;
+            this.opMulti.Click += new System.EventHandler(this.opMulti_Click);
+            // 
+            // opResta
+            // 
+            this.opResta.Location = new System.Drawing.Point(392, 248);
+            this.opResta.Name = "opResta";
+            this.opResta.Size = new System.Drawing.Size(114, 58);
+            this.opResta.TabIndex = 13;
+            this.opResta.Text = "-";
+            this.opResta.UseVisualStyleBackColor = true;
+            this.opResta.Click += new System.EventHandler(this.opResta_Click);
+            // 
+            // opSuma
+            // 
+            this.opSuma.Location = new System.Drawing.Point(392, 184);
+            this.opSuma.Name = "opSuma";
+            this.opSuma.Size = new System.Drawing.Size(114, 58);
+            this.opSuma.TabIndex = 12;
+            this.opSuma.Text = "+";
+            this.opSuma.UseVisualStyleBackColor = true;
+            this.opSuma.Click += new System.EventHandler(this.opSuma_Click);
+            // 
+            // opIgual
+            // 
+            this.opIgual.Location = new System.Drawing.Point(272, 376);
+            this.opIgual.Name = "opIgual";
+            this.opIgual.Size = new System.Drawing.Size(114, 58);
+            this.opIgual.TabIndex = 11;
+            this.opIgual.Text = "=";
+            this.opIgual.UseVisualStyleBackColor = true;
+            this.opIgual.Click += new System.EventHandler(this.opIgual_Click);
+            // 
+            // cajaResultado
+            // 
+            this.cajaResultado.Enabled = false;
+            this.cajaResultado.Font = new System.Drawing.Font("BIZ UDMincho Medium", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cajaResultado.Location = new System.Drawing.Point(32, 30);
+            this.cajaResultado.MinimumSize = new System.Drawing.Size(200, 100);
+            this.cajaResultado.Name = "cajaResultado";
+            this.cajaResultado.Size = new System.Drawing.Size(475, 79);
+            this.cajaResultado.TabIndex = 17;
+            // 
+            // listHistorial
+            // 
+            this.listHistorial.FormattingEnabled = true;
+            this.listHistorial.ItemHeight = 20;
+            this.listHistorial.Location = new System.Drawing.Point(532, 30);
+            this.listHistorial.Name = "listHistorial";
+            this.listHistorial.Size = new System.Drawing.Size(192, 404);
+            this.listHistorial.TabIndex = 18;
+            this.listHistorial.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listMemoria
+            // 
+            this.listMemoria.FormattingEnabled = true;
+            this.listMemoria.ItemHeight = 20;
+            this.listMemoria.Location = new System.Drawing.Point(740, 30);
+            this.listMemoria.Name = "listMemoria";
+            this.listMemoria.Size = new System.Drawing.Size(126, 404);
+            this.listMemoria.TabIndex = 19;
+            this.listMemoria.DoubleClick += new System.EventHandler(this.listMemoria_DoubleClick);
+            // 
+            // opDel
+            // 
+            this.opDel.Location = new System.Drawing.Point(393, 136);
+            this.opDel.Name = "opDel";
+            this.opDel.Size = new System.Drawing.Size(114, 42);
+            this.opDel.TabIndex = 20;
+            this.opDel.Text = "Del";
+            this.opDel.UseVisualStyleBackColor = true;
+            this.opDel.Click += new System.EventHandler(this.opDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 448);
+            this.Controls.Add(this.opDel);
+            this.Controls.Add(this.listMemoria);
+            this.Controls.Add(this.listHistorial);
+            this.Controls.Add(this.cajaResultado);
+            this.Controls.Add(this.opDivi);
+            this.Controls.Add(this.opMulti);
+            this.Controls.Add(this.opResta);
+            this.Controls.Add(this.opSuma);
+            this.Controls.Add(this.opIgual);
+            this.Controls.Add(this.opMemoria);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button opMemoria;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button opDivi;
+        private System.Windows.Forms.Button opMulti;
+        private System.Windows.Forms.Button opResta;
+        private System.Windows.Forms.Button opSuma;
+        private System.Windows.Forms.Button opIgual;
+        private System.Windows.Forms.TextBox cajaResultado;
+        private System.Windows.Forms.ListBox listHistorial;
+        private System.Windows.Forms.ListBox listMemoria;
+        private System.Windows.Forms.Button opDel;
+    }
+}
+
---
+++ ProyectoCalculadora/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCalculadora
+{
+    public partial class Form1 : Form
+    {
+        public int op1,op2,tipoOp;
+        public string cadOpe1, cadOpe2;
+        public Boolean opDif=true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Botones numericos
+        private void num0_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "0";
+            calculaDigitos();
+
+        }
+        private void num1_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "1";
+            calculaDigitos();
+        }
+        private void num2_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "2";
+            calculaDigitos();
+        }
+        private void num3_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "3";
+            calculaDigitos();
+        }
+        private void num4_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "4";
+            calculaDigitos();
+        }
+        private void num5_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "5";
+            calculaDigitos();
+        }
+        private void num6_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "6";
+            calculaDigitos();
+        }
+        private void num7_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "7";
+            calculaDigitos();
+        }
+        private void num8_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "8";
+            calculaDigitos();
+        }
+        private void num9_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = cajaResultado.Text + "9";
+            calculaDigitos();
+        }
+
+        //Funciones generales
+        private int calculaDigitos()
+        {
+            //Se escriben las cadenas para calcular su longitud individual
+            if (opDif)
+            {
+                cadOpe1=cajaResultado.Text;
+                if (cadOpe1.Equals(""))
+                {
+                    MessageBox.Show("Valor vacio");
+                    return 0;
+                }
+                else
+                {
+                    try { 
+                        op1 = int.Parse(cajaResultado.Text);
+                        return 1;
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Valor no valido, solo numeros");
+                        cajaResultado.Text = "";
+                        return 0;
+                    }
+                }
+                
+            }
+            else
+            {
+                cadOpe2 = cajaResultado.Text.Remove(0,cadOpe1.Length+1);
+                if (cadOpe2.Equals(""))
+                {
+                    MessageBox.Show("Valor vacio");
+                    return 0;
+                }
+                else
+                {
+                    try
+                    {
+                        op2 = int.Parse(cadOpe2);
+                        return 1;
+                    }
+                    catch(Exception e)
+                    {
+                        MessageBox.Show("Valor no valido, solo numeros");
+                        cajaResultado.Text = "";
+                        return 0;
+                    }
+                }
+                
+            }
+        }
+
+        //Botones Operadores(op)
+        private void opDel_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = "";
+            opDif = true;
+            op1 = 0;
+            op2 = 0;
+            cadOpe1 = "";
+            cadOpe2 = "";
+        }
+
+        //Suma es el tipo de operacion 1
+        private void opSuma_Click(object sender, EventArgs e)
+        {
+            if (opDif)
+            {
+                //Define el primer operador
+                if (calculaDigitos() == 1)
+                {
+                    cajaResultado.Text += "+";
+                    tipoOp = 1;
+                    opDif = false;
+                }
+            }
+            else
+            {
+                //Por si usa el simbolo en lugar del igual
+                if (calculaDigitos() == 1)
+                {
+                    int resul = hazOperacion();
+                    op1 = resul;
+                    cadOpe1 = resul.ToString();
+
+                    listHistorial.Items.Add(cajaResultado.Text + "=" + resul);
+                    cajaResultado.Text = resul + "";
+                    opDif = true;
+                    calculaDigitos();
+                    cajaResultado.Text += "+";
+                    tipoOp = 1;
+                    opDif = false;
+                }
+            }
+        }
+
+        //Resta es el tipo de operacion 2
+        private void opResta_Click(object sender, EventArgs e)
+        {
+            if (opDif)
+            {
+                //Define el primer operador
+                if (calculaDigitos() == 1)
+                {
+                    cajaResultado.Text += "-";
+                    tipoOp = 2;
+                    opDif = false;
+                }
+            }
+            else
+            {
+                //Por si usa el simbolo en lugar del igual
+                if (calculaDigitos() == 1)
+                {
+                    int resul = hazOperacion();
+                    op1 = resul;
+                    cadOpe1 = resul.ToString();
+
+                    listHistorial.Items.Add(cajaResultado.Text + "=" + resul);
+                    cajaResultado.Text = resul + "";
+                    opDif = true;
+                    calculaDigitos();
+                    cajaResultado.Text += "-";
+                    tipoOp = 2;
+                    opDif = false;
+                }
+            }
+        }
+
+        //Multiplicacion es el tipo de operacion 3
+        private void opMulti_Click(object sender, EventArgs e)
+        {
+            if (opDif)
+            {
+                //Define el primer operador
+                if (calculaDigitos() == 1)
+                {
+                    cajaResultado.Text += "*";
+                    tipoOp = 3;
+                    opDif = false;
+                }
+            }
+            else
+            {
+                //Por si usa el simbolo en lugar del igual
+                if (calculaDigitos() == 1)
+                {
+                    int resul = hazOperacion();
+                    op1 = resul;
+                    cadOpe1 = resul.ToString();
+
+                    listHistorial.Items.Add(cajaResultado.Text + "=" + resul);
+                    cajaResultado.Text = resul + "";
+                    opDif = true;
+                    calculaDigitos();
+                    cajaResultado.Text += "*";
+                    tipoOp = 3;
+                    opDif = false;
+                }
+            }
+        }
+
+
+        //Division es el tipo de operacion 4
+        private void opDivi_Click(object sender, EventArgs e)
+        {
+            if (opDif)
+            {
+                //Define el primer operador
+                if (calculaDigitos() == 1)
+                {
+                    cajaResultado.Text += "/";
+                    tipoOp = 4;
+                    opDif = false;
+                }
+            }
+            else
+            {
+                //Por si usa el simbolo en lugar del igual
+                if (calculaDigitos() == 1)
+                {
+                    int resul = hazOperacion();
+                    op1 = resul;
+                    cadOpe1 = resul.ToString();
+
+                    listHistorial.Items.Add(cajaResultado.Text + "=" + resul);
+                    cajaResultado.Text = resul + "";
+                    opDif = true;
+                    calculaDigitos();
+                    cajaResultado.Text += "/";
+                    tipoOp = 4;
+                    opDif = false;
+                }
+            }
+        }
+
+
+        //Simbolo igual
+        private void opIgual_Click(object sender, EventArgs e)
+        {
+            if (calculaDigitos() == 1)
+            {
+                //Define el segundo operador
+                int resul = hazOperacion();
+                op1 = resul;
+                cadOpe1 = resul.ToString();
+                cadOpe2 = "";
+
+                listHistorial.Items.Add(cajaResultado.Text + "=" + resul);
+                cajaResultado.Text = resul + "";
+                opDif = true;
+                calculaDigitos();
+            }
+        }
+
+       //El switch que usa los dos operadores leidos
+        private int hazOperacion()
+        {
+            int res=0;
+            switch (tipoOp)
+            {
+                case 1:
+                    res = op1 + op2; break;
+                case 2:
+                    res = op1 - op2; break;
+                case 3:
+                    res = op1 * op2; break;
+                case 4:
+                    res = op1 / op2; break;
+                default: MessageBox.Show("Operacion no valida");
+                    cajaResultado.Text = "";
+                    break;
+            }
+            return res;
+        }
+
+        private void opMemoria_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int mem = int.Parse(cajaResultado.Text);
+                listMemoria.Items.Add(mem);
+            }
+            catch(Exception ex) { MessageBox.Show("Este no es un valor valido"); }
+            
+            
+        }
+
+
+        private void listMemoria_DoubleClick(object sender, EventArgs e)
+        {
+
+
+            if (sender.Equals(""))
+            {
+                MessageBox.Show("No hay valor");
+            }
+            else
+            {
+                //Recuperar la memoria primer valor
+                if (opDif)
+                {
+                    cajaResultado.Text = "";
+                    op1 = 0;
+                    op2 = 0;
+                    cadOpe1 = "";
+                    cadOpe2 = "";
+                    cajaResultado.Text = cajaResultado.Text + listMemoria.Items[listMemoria.SelectedIndex];
+                    cadOpe1 = cajaResultado.Text;
+
+
+                }
+                else
+                {
+                    //Recuperar la memoria segundo valor
+                    cajaResultado.Text = cajaResultado.Text + listMemoria.Items[listMemoria.SelectedIndex];
+
+                    op2 = int.Parse(cajaResultado.Text.Remove(0, cadOpe1.Length + 1));
+                    
+                  
+                }
+            }
+            
+        }
+
+    }
+}
